5e9bbc0345c2e4905fff8119b0fd15fa92c88a5a
najibdevs <najibsoftwareeng@gmail.com>
2024-04-12 21:31:02 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  main.innerHTML = ''; // Clear previous content
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
